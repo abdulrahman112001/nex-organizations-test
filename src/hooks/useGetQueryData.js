64e85ce1a45763cf22bb8729d314146deabd3398
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-03-17 10:35:45 +0200

    updates

--- src/hooks/useGetQueryData.js
+++
@@ -1,5 +0,0 @@
-import { useQueryClient } from "@tanstack/react-query"
-
-export const useGetQueryData = <T>(key: [string] | [string, string] | [string, number]) => {
-    return useQueryClient().getQueryData<T>(key)
-}
